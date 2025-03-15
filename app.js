e05a3dcf840665dbfb8e73cf852fdf9eb4667fc4
AduragbemiShobowale <shobowaleaduragbemi24@gmail.com>
2025-03-15 22:50:41 +0100

    first commit

--- app.js
+++ app.js
@@ -22,15 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "views"));
